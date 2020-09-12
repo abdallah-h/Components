f74e6ef08b57043ee561957d9adf164f2961db3e
Abdallah <abdallah-h@abdallah.github.com>
2020-09-12 23:14:35 +0200

    initial commits

---
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collections-home',
+  templateUrl: './collections-home.component.html',
+  styleUrls: ['./collections-home.component.css'],
+})
+export class CollectionsHomeComponent implements OnInit {
+  classNames = 'striped celled';
+  data = [
+    { name: 'abdallah', age: 24, job: 'Developer', employed: true },
+    { name: 'abdallah', age: 25, job: 'Developer', employed: false },
+    { name: 'abdallah', age: 26, job: 'Developer', employed: true },
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+    { key: 'employed', label: 'Employed' },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
