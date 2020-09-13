31b27da6274286abd7786d1ad07bed2ecee0ede3
Abdallah <abdohfox17@gmail.com>
2020-09-13 16:45:30 +0200

    add Modal

--- src/app/views/item-list/item-list.component.ts
+++ src/app/views/item-list/item-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.css']
+  styleUrls: ['./item-list.component.css'],
 })
 export class ItemListComponent implements OnInit {
+  @Input() items = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
